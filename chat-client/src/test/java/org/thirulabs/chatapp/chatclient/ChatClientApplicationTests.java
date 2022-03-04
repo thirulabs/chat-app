b901a6a01677f7f443642e6d7ac2fe6a10a013ed
Thiru <84318287+thirulabs@users.noreply.github.com>
2022-03-04 22:03:39 +0530

    Initial Commit

---
+++ chat-client/src/test/java/org/thirulabs/chatapp/chatclient/ChatClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thirulabs.chatapp.chatclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ chat-server/src/test/java/org/thirulabs/chatapp/chatserver/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thirulabs.chatapp.chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
