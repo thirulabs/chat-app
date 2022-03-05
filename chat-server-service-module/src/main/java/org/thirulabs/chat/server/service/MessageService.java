e9eacc58b98baa49767207b3cf2764bd0d5d9b31
Thiru <84318287+thirulabs@users.noreply.github.com>
2022-03-05 10:19:56 +0530

    Added spring boot dependency management

---
+++ chat-server-service-module/src/main/java/org/thirulabs/chat/server/service/MessageService.java
@@ -0,0 +1,42 @@
+package org.thirulabs.chat.server.service;
+
+import org.thirulabs.chat.commons.Message;
+
+import java.util.List;
+
+public interface MessageService {
+    /**
+     * Finds message by id
+     * @param id of message
+     * @return Message if success, or empty when not found
+     * Note: Could have used Optional here
+     */
+    Message findById(Long id);
+
+    /**
+     * Finds all messages
+     * @return list of messages
+     */
+    List<Message> findAll();
+
+    /**
+     * Saves message
+     * @param message message
+     * @return saved message
+     */
+    Message add(Message message);
+    /**
+     * Returns true if update is success
+     * @param id of message
+     * @param message message
+     * @return on success
+     */
+    boolean update(Long id, Message message);
+
+    /**
+     * Returns true if removal is success
+     * @param id of message
+     * @return true on success
+     */
+    boolean remove(Long id);
+}
---
+++ chat-server-service-module/src/main/java/org/thirulabs/chat/server/service/inmemory/InMemoryMessageService.java
@@ -0,0 +1,49 @@
+package org.thirulabs.chat.server.service.inmemory;
+
+
+import org.springframework.stereotype.Service;
+import org.thirulabs.chat.commons.Message;
+import org.thirulabs.chat.server.service.MessageService;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class InMemoryMessageService implements MessageService {
+    private Map<Long, Message> messageMap = new ConcurrentHashMap<>();
+    private AtomicLong idGenerator = new AtomicLong(0);
+    @Override
+    public Message findById(Long id) {
+        return messageMap.get(id);
+    }
+
+    @Override
+    public List<Message> findAll() {
+        return List.copyOf(messageMap.values());
+    }
+
+    @Override
+    public Message add(Message message) {
+        //generate id before saving it to map
+        Long id = idGenerator.incrementAndGet();
+        message.setId(id);
+        return messageMap.put(id, message);
+    }
+
+    @Override
+    public boolean update(Long id, Message message) {
+        if(id!=null && message!=null && id.equals(message.getId())){
+            messageMap.replace(id, message);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean remove(Long id) {
+        //remove method returns the object removed, returns null when key is not found
+        return messageMap.remove(id) != null;
+    }
+}
